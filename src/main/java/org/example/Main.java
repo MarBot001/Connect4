596772a6cff37d7c7a1727391770aec083e148e2
MarBot001 <118659314+MarBot001@users.noreply.github.com>
2024-12-27 10:22:39 +0100

    3. commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello worldd!");
+        System.out.println("Hello worlddddd!");
     }
 }
